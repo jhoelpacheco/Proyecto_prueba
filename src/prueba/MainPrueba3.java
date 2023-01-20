fd2d77e42abe842ccb03db535372f6754465ce58
FahadToqir <cf19fahad.toqir@iesjoandaustria.org>
2023-01-20 15:19:00 +0100

    prueba2json

---
+++ src/prueba/MainPrueba3.java
@@ -0,0 +1,39 @@
+package prueba;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+ 
+
+public class MainPrueba3 {
+
+	public static void main(String[] args) throws Exception{
+		String churro = "[\r\n"
+				+ "{\"hora_ini\":\"14:30\",\"hora_fin\":\"16:30\",\"modulo\":\"CFPROJ\",\"curso\":\"ASIX2/DAM2/DAW2\"},\r\n"
+				+ "{\"hora_ini\":\"16:30\",\"hora_fin\":\"18:30\",\"modulo\":\"DAMM09\",\"curso\":\"DAM2\"}\r\n"
+				+ "]\r\n"
+				+ "";
+		// Instantiate the Factory
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String conex = "jdbc:mysql://localhost:3306/pruebaxml_a_java";
+		Connection conexion = DriverManager.getConnection(conex, "root", "");
+
+		/*String sql = "INSERT INT horarios (profe,martes) VALUES (?, ?);";
+		
+		PreparedStatement sentencia = conexion.prepareStatement(sql);
+				sentencia.setString(1, "EDUARD");
+				sentencia.setString(2, churro);
+				System.out.print(sentencia);
+				int filas = sentencia.executeUpdate();
+				System.out.print("Filas afectadas: " + filas);
+		*/
+		
+         String sql = "SELECT * FROM horarios WHERE profe='EDUARD';";
+		
+		 PreparedStatement sentencia = conexion.prepareStatement(sql);
+		 System.out.print(sentencia);
+			
+		sentencia.close();
+		conexion.close();
+	}
+}
